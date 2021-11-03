14ea86c20747bd15bbc150337c08e4dbd832c88e
Md Mosleh Uddin <49858741+M0hammadASAD@users.noreply.github.com>
2021-11-03 12:05:19 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,140 +5,245 @@ from datetime import datetime
 from PIL import Image
 import json
 import base64
+from streamlit.state.session_state import SessionState
 import yagmail
 import re
 from re import search
-import smtplib
- 
+# import smtplib
+import datetime
+import uuid
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit import caching
- 
+
 import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from sqlalchemy import create_engine
 from mysql.connector.constants import ClientFlag
-from uuid import uuid4
+
 import yaml
-from db_connection import get_database_connection
+import mysql.connector as mysql
+from mysql.connector.constants import ClientFlag
+from sqlalchemy import create_engine
 
 st.set_page_config(
-    page_title="Admission Form",
-    page_icon=":sunny:",
-    # layout="wide",
+    page_title="Data Science Bootcamp",
+    page_icon=":smiley:",
     initial_sidebar_state="expanded",
 )
-# database localhost connection
-# @st.cache()
 
-# def get_database_connection():
-#     db = mysql.connect(host = "localhost",
-#                       user = "root",
-#                       passwd = "root",
-#                       database = "mydatabase",
-#                       auth_plugin='mysql_native_password')
-#     cursor = db.cursor()
-#     return cursor, db
- 
+
+
+with open('credintials.yml', 'r') as f:
+    credintials = yaml.load(f, Loader=yaml.FullLoader)
+    db_credintials = credintials['db']
+    system_pass = credintials['system_pass']['admin']
+    # email_sender = credintials['email_sender']
+
+
+def get_database_connection():
+    db = mysql.connect(host = db_credintials['host'],
+                      user = db_credintials['user'],
+                      passwd = db_credintials['passwd'],
+                      database = db_credintials['database'],
+                      auth_plugin= db_credintials['auth_plugin'])
+    cursor = db.cursor()
+
+    return cursor, db
+
+if 'authenticated' not in st.session_state:
+    st.session_state.authenticated = False
+if 'login' not in st.session_state:
+    st.session_state.login = False
+
 cursor, db = get_database_connection()
- 
-# cursor.execute("SHOW DATABASES")
- 
-# databases = cursor.fetchall() ## it returns a list of all databases present
- 
-# st.write(databases)
- 
-# cursor.execute('''CREATE TABLE information (id varchar(255),
-#                                               studentname varchar(255),
-#                                               re_date date,
-#												status varchar(255))''')
-# cursor.execute("Select * from information")
-# tables = cursor.fetchall()
-# st.write(tables)
 
-def admin():
-    username=st.sidebar.text_input('Username',key='user')
-    password=st.sidebar.text_input('Password',type='password',key='pass')
-    st.session_state.login=st.sidebar.checkbox('Login')
- 
-    if st.session_state.login==True:
-        if username=="name" and password=='password':
-            st.sidebar.success('Login Success')
+mp = {
+    0: "Tracking ID",
+    1: "Full Name",
+    2: "Age",
+    3: "Phone",
+    4: "E-mail",
+    5: "City",
+    6: "Address",
+    7: "Hours Per Week",
+    8: "Current Status",
+    9: "GPA",
+    10: "Password",
+    11: "Application Date",
+    12: "Gender"
+}
+def view(var):
+    cursor.execute(
+            f"select * from db_bootcamp.users")
+    tables = cursor.fetchall()
+    for i in tables:
+        if i[8]==var:
+            with st.expander(i[1]):
+                j = 0
+                while j < 12:
+                    tmp = i[j]
+                    tmp = str(tmp)
+                    st.metric(label=mp[j], value=tmp)
+                    j += 1
 
-            date1=st.date_input('Date1')
-            date2=st.date_input('Date2')
-            cursor.execute(f"select * from information where re_date between '{date1}' and '{date2}'")
-            # db.commit()
-            tables =cursor.fetchall()
-            # st.write(tables)
-            for i in tables:
-                st.write(i[1])
-                st.write(i[2])
-                Accept=st.button('Accept',key=i[0])
-                if Accept:
-                    st.write('Accepted')
-                    cursor.execute(f"Update information set status='Accepted' where id='{i[0]}'")
-                    db.commit()
-                Reject=st.button('Reject',key=i[0])
-                if Reject:
-                    st.write('Rejected')
-                    cursor.execute(f"Update information set status='Rejected' where id='{i[0]}'")
-                    db.commit()
+def pending():
+    cursor.execute(
+            f"select * from db_bootcamp.users")
+    tables = cursor.fetchall()
+    for i in tables:
+        if i[8]=='In queue':
+            with st.expander(i[1]):
+                j = 0
+                flag = False
+                while j < 12:
+                    tmp = i[j]
+                    tmp = str(tmp)
+                    if j == 8 and tmp == 'In queue':
+                        flag = True
+                    st.metric(label=mp[j], value=tmp)
+                    j += 1
+                if flag:
+                    col = st.columns(6)
+                    accept = col[4].button("Accept",key=i[0])
+                    reject = col[5].button("Reject",key=i[0])
+                    if accept:
+                        st.balloons()
+                        st.success('Accepted')
+                        cursor.execute(f"Update db_bootcamp.users set cur_status='Accepted' where track_id='{i[0]}'")
+                        db.commit()
+                    if reject:
+                        st.warning('Rejected')
+                        cursor.execute(f"Update db_bootcamp.users set cur_status='Rejected' where track_id='{i[0]}'")
+                        db.commit()
+                    
+    
 
+def disp():
+    selection = st.sidebar.selectbox('Select View Option:', [
+        'View in range of some Date', 'View Pendings', 'View Accepted', 'View Rejected'])
+    if selection == 'View in range of some Date':
+        st.session_state.login = True
+        col0 = st.columns(2)
+        date1 = col0[0].date_input("Start Date:")
+        date2 = col0[1].date_input("End Date:")
+        cursor.execute(
+            f"select * from db_bootcamp.users where reg_date between '{date1}' and '{date2}'")
+        tables = cursor.fetchall()
+        for i in tables:
+            with st.expander(i[1]):
+                j = 0
+                flag = False
+                while j < 12:
+                    tmp = i[j]
+                    tmp = str(tmp)
+                    if j == 8 and tmp == 'In queue':
+                        flag = True
+                    st.metric(label=mp[j], value=tmp)
+                    j += 1
+                if flag:
+                    col = st.columns(6)
+                    accept = col[4].button("Accept",key=i[0])
+                    reject = col[5].button("Reject",key=i[0])
+                    if accept:
+                        st.balloons()
+                        st.success('Accepted')
+                        cursor.execute(f"Update db_bootcamp.users set cur_status='Accepted' where track_id='{i[0]}'")
+                        db.commit()
+                    if reject:
+                        st.warning('Rejected')
+                        cursor.execute(f"Update db_bootcamp.users set cur_status='Rejected' where track_id='{i[0]}'")
+                        db.commit()
+    elif selection=='View Pendings':
+        pending()
+    elif selection=='View Accepted':
+        view('Accepted')
+    elif selection=='View Rejected':
+        view('Rejected')
+
+
+
+def login():
+    username = st.sidebar.text_input('Username', key='user')
+    password = st.sidebar.text_input('Password', type='password', key='pass')
+    st.session_state.login = st.sidebar.checkbox('Sign In')
+    if st.session_state.login:
+        cursor.execute(
+            f"select * from db_bootcamp.admin where username='Admin'")
+        tables = cursor.fetchall()
+        if username == tables[0][0] and password == tables[0][1]:
+            st.session_state.login = True
+            disp()
         else:
-            st.sidebar.warning('Wrong Credintials')
+            st.sidebar.warning("Wrong Credintials")
 
 
-def form():
-    id=uuid4()
-    id=str(id)[:10]
-    with st.form(key='member form'):
-        sname=st.text_input('Student Name')
-        re_date=st.date_input('Registration Date')
-        status='In Progress'
+def apply():
+    track_id = uuid.uuid1()
+    track_id = str(track_id)[:18]
+    with st.form(key="apply_form"):
+        full_name = st.text_input("Enter your Full Name : ", "Sherlock Holmes")
+        pwd = st.text_input(
+            "Enter a Password to See Your Application Status in future :", "SHERLOCKED", type='password', key='pas')
+        age = st.number_input("Enter your age : ", 18)
+        gender = st.radio("Select Gender: ", ['Male', 'Female', 'Other'])
+        phone = st.text_input("Enter Phone No. : ", "+0145678945")
+        email = st.text_input("Enter your E-mail : ", "info@shelock.me")
+        city = st.selectbox('Select the city you live in : ', ['-------',
+                                                               'Dhaka', 'Chattogram', 'Khulna', 'Rajshahi', 'Barisal', 'Rangpur'])
+        address = st.text_area("Enter your Address : ",
+                               " 221b Baker St, London NW1 6XE, United Kingdom")
+        hours_per_week = st.slider(
+            "How may hours you can work in a Week?", min_value=0, max_value=100)
+        cur_status = "In queue"
+        gpa = st.text_input("Enter your GPA :", "4.00")
+        reg_date = datetime.date.today()
         if st.form_submit_button('Submit'):
-            query = f'''INSERT INTO information (id,studentname,
-                                                re_date,status) VALUES ('{id}','{sname}',
-                                                '{re_date}','{status}')'''
+            query = f'''INSERT INTO db_bootcamp.users (track_id,full_name,age,phone,email,city,address,hours_per_week,cur_status,gpa,pwd,reg_date,gender)
+                            VALUES ('{track_id}','{full_name}','{age}','{phone}','{email}','{city}','{address}','{hours_per_week}','{cur_status}','{gpa}','{pwd}','{reg_date}','{gender}')'''
             cursor.execute(query)
             db.commit()
-            st.success(f'Congratulation *{sname}*! You have successfully Registered')
-            st.code(id)
-            st.warning("Please Store this code!!!")
-        
-def info():
-    id=st.text_input('Your Code')
-    Submit=st.button(label='Search')
-    if Submit:
-    	cursor.execute(f"select * from information where id='{id}'")
-    	tables = cursor.fetchall()
-    	st.write(tables)
+            st.success(
+                f'Congratulation *{full_name}*! Your Application has been received.')
+            st.code(track_id)
+            st.warning("Please Store this code!!! To Check your application status please go to Track Status tab from Side Bar and Enter the Tracking ID and password which you have provided on the form and Hit enter.")
 
-def stat():
-    id=st.text_input('Your Id')
-    submit=st.button('Search',key='sub')
-    if submit:
-        cursor.execute(f"Select status from information where id='{id}'")
-        table=cursor.fetchall()
-        st.write(table)
+
+def check_stat():
+    track_id = st.text_input('Enter Your Tracking ID :')
+    pwd = st.text_input('Enter Your Password :')
+    Submit = st.button(label='Check Status')
+    if Submit:
+        cursor.execute(
+            f"select * from db_bootcamp.users where track_id='{track_id}' and pwd='{pwd}'")
+        tables = cursor.fetchall()
+        if len(tables) == 0:
+            st.warning("Wrong Credintials. Try Again")
+        else:
+            if tables[0][8] == 'In queue':
+                st.info(
+                    f'Dear *{tables[0][1]}*,\n Your application is In Queue. Please Wait. We will get back to you soon.')
+            elif tables[0][8] == 'Accepted':
+                st.balloons()
+                st.success(
+                    f'Dear *{tables[0][1]}*,\n Congratulations!!!You are selected for the Data Science bootcamp.')
+            else:
+                st.warning(
+                    f'Dear *{tables[0][1]}*, \nWe are sorry. You have been rejected. Better Luck next time.')
+
 
 def main():
-    st.title('Diploma in Data Science Admission')
-    selected=st.sidebar.selectbox('Select',
-                        ('-----------',
-                        'Admin',
-                        'Registration',
-                        'Information',
-                        'Status'
-                        ))
-    if selected=='Admin':
-        admin()
-    elif selected=='Registration':
-        form()
-    elif selected=='Information':
-        info()
-    elif selected=='Status':
-        stat()
-if __name__=='__main__':
+    st.header("Welcome to Data Science Bootcamp!!!")
+    st.sidebar.subheader("Please Select One :")
+    menu_choice = st.sidebar.selectbox(
+        '', ['---------', 'Apply for a Slot', 'Admin Login', 'Track Status'])
+    if menu_choice == "Admin Login":
+        login()
+    elif menu_choice == "Apply for a Slot":
+        apply()
+    elif menu_choice == 'Track Status':
+        check_stat()
+
+if __name__ == '__main__':
     main()
